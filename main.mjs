f326c755756600bcf17aae1fe95bd543d936cf47
Caique de Castro Soares da Silva <caique.castro@venturus.org.br>
2022-11-09 18:09:01 -0300

    Initial commit

---
+++ main.mjs
@@ -0,0 +1,11 @@
+import Koa from 'koa';
+import { koaBody } from 'koa-body';
+
+const app = new Koa();
+
+app.use(koaBody());
+app.use((ctx) => {
+  ctx.body = ctx.request.body;
+});
+
+app.listen(3000);
