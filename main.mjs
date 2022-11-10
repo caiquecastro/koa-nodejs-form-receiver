5c0ba662a2aadf8da86536c2b755f2faee69db63
Caique de Castro Soares da Silva <caique.castro@venturus.org.br>
2022-11-10 13:54:26 -0300

    Add default PORT

--- main.mjs
+++ main.mjs
@@ -3,9 +3,11 @@ import { koaBody } from 'koa-body';
 
 const app = new Koa();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(koaBody());
 app.use((ctx) => {
   ctx.body = ctx.request.body;
 });
 
-app.listen(process.env.PORT);
+app.listen(PORT);
