7ebbc2e19cd5e404a9542acef0d4a7c4dab051da
Caique de Castro <caique.castro@venturus.org.br>
2022-11-12 15:50:40 -0300

    Add prettier

--- test/app.test.mjs
+++ test/app.test.mjs
@@ -3,37 +3,39 @@ import request from 'supertest';
 import { assert } from 'node:assert';
 
 describe('App', () => {
-    let server;
+  let server;
 
-    before(() => {
-        server = app.listen();
-        console.log = () => {};
+  before(() => {
+    server = app.listen();
+    console.log = () => {};
+  });
+
+  after(() => {
+    server.close();
+  });
+
+  it('should load all parameters from body', async () => {
+    const response = await request(server).post('/').send('name=John&age=20');
+
+    assert.deepStrictEqual(Object.keys(response.body), [
+      'method',
+      'headers',
+      'body',
+    ]);
+    assert.deepStrictEqual(response.body.method, 'POST');
+    assert.deepStrictEqual(response.body.body, {
+      name: 'John',
+      age: '20',
     });
+  });
 
-    after(() => {
-        server.close();
+  it('should load multiple fields with same name as array', async () => {
+    const response = await request(server)
+      .post('/')
+      .send('name=value1&name=value2');
+
+    assert.deepStrictEqual(response.body.body, {
+      name: ['value1', 'value2'],
     });
-
-    it('should load all parameters from body', async () => {
-        const response = await request(server)
-            .post('/')
-            .send('name=John&age=20');
-
-        assert.deepStrictEqual(Object.keys(response.body), ['method', 'headers', 'body']);
-        assert.deepStrictEqual(response.body.method, 'POST');
-        assert.deepStrictEqual(response.body.body, {
-            name: 'John',
-            age: '20',
-        });
-    });
-
-    it('should load multiple fields with same name as array', async () => {
-        const response = await request(server)
-            .post('/')
-            .send('name=value1&name=value2');
-
-        assert.deepStrictEqual(response.body.body, {
-            name: ['value1', 'value2'],
-        });
-    });
+  });
 });
