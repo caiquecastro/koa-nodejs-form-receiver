6af49845616e68645711807f98d78cacbef15ec0
Caique de Castro <caique.castro@venturus.org.br>
2022-11-22 15:12:58 -0300

    feat: Add CORS

--- app.mjs
+++ app.mjs
@@ -1,9 +1,11 @@
 import Koa from 'koa';
 import { koaBody } from 'koa-body';
+import cors from '@koa/cors';
 
 export const app = new Koa();
 
 app.use(koaBody({}));
+app.use(cors());
 app.use((ctx) => {
   const { body, method, headers } = ctx.request;
   console.log(body);
